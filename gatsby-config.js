f9f9da64b2c1ddda3ba916d9a1b71941baaaf792
Hashim Warren <hashimwarren@gmail.com>
2020-01-11 16:40:34 -0500

    Change site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Full Stack Marketer`,
+    description: `Hashim Warren's code playground`,
+    author: `@hashim_warren`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
